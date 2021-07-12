bcb20e4bfe56ba28f3a9f01377bd3d8de3437e99
Joao Chaves <chavesjj@outlook.com>
2021-07-12 00:40:49 -0300

    fix migrate client

--- db/migrate/20210711223428_create_clients.rb
+++ db/migrate/20210711223428_create_clients.rb
@@ -2,8 +2,7 @@ class CreateClients < ActiveRecord::Migration[6.1]
   def change
     create_table :clients do |t|
       t.string :full_name
-      t.string :rg
-      t.string :cpf
+      t.string :document
       t.string :cellphone
       t.string :address
       t.string :complement
