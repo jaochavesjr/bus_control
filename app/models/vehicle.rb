97fc32268bb036882f6df3b2798c704504ea9beb
Joao Chaves <chavesjj@outlook.com>
2021-08-05 12:45:49 -0300

    FIX crud Vehicles api

--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Vehicle < ApplicationRecord
-  validates :pĺate, uniqueness: true, presence: true
+  validates :plate, uniqueness: true, presence: true
   validates :model_description, presence: true
   validates :year, presence: true
   validates :toilet, presence: true
